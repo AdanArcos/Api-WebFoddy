9fb905c18a0bedd4ad94a3ee183ea751948c981e
Adan Arcos <cheosarao2000@gmail.com>
2023-05-23 18:27:23 -0600

    sexo

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const port =process.env.PORT || 4000;
 
 
 mongoose.set("strictQuery",false);
-mongoose.connect(`mongodb://${IP_SERVER}:${DB_PORT}/${DB_NAME}`, (error)=>{
-    if(error) throw error;
+mongoose.connect(`mongodb+srv://cheosarao2000:EF3HgdS3bOiqgflc@cluster0.y47ux2l.mongodb.net/${DB_NAME}`, (error)=>{
+    if(error) {console.log(error);throw error};
 })
 
 
